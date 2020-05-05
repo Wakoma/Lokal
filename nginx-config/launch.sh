8f5a2ede2a1c2d61bcb8ac44e72684abe47dbf04
Mattthew <matthewdtait01@gmail.com>
2020-05-05 16:45:33 +0200

    update nginx config default server

--- nginx-config/launch.sh
+++
@@ -1,3 +0,0 @@
-docker run --rm -d --name nginx-test -v $PWD/nginx:/etc/nginx --network vnet-vlan nginx
-docker network connect bridge nginx-test
-
---
+++ nginx/launch.sh
@@ -0,0 +1,2 @@
+docker run --rm -d -p 80:80 --name nginx-test -v $PWD/nginx:/etc/nginx nginx
+
