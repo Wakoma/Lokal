a2682b1c31a7a5492b5ae973c68c6beb84accae5
Matthew Tait <matthewdtait01@gmail.com>
2020-05-07 19:27:24 +0200

    fixed wordpress assets

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -1,2 +1,2 @@
-docker run --rm -d -p 80:80 --name nginx-test -v $PWD/nginx:/etc/nginx nginx
+docker run --rm -d -p 80:80 --network testnet  --name nginx-test -v $PWD/nginx:/etc/nginx nginx
 
---
+++ transmission/launch.sh
@@ -0,0 +1,18 @@
+docker run \
+  --name=transmission \
+  --network testnet \
+  -d \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/London \
+  -e TRANSMISSION_WEB_HOME=/combustion-release/ `#optional` \
+  -e USER=username `#optional` \
+  -e PASS=password `#optional` \
+  -p 9091:9091 \
+  -p 51413:51413 \
+  -p 51413:51413/udp \
+  -v $PWD/config:/config \
+  -v $PWD/downloads:/downloads \
+  -v $PWD/watch:/watch \
+  --restart unless-stopped \
+  linuxserver/transmission
