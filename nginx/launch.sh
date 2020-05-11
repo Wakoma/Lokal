cf66265b900fe1c03e72a5f189c722255b374628
Mattthew <matthewdtait01@gmail.com>
2020-05-11 17:14:05 +0200

    incorrect flag removed

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -1,2 +1,2 @@
-docker run --rm -d -p 80:80 --restart unless-stopped --network vpuu-offline  --name nginx -v $PWD/nginx:/etc/nginx nginx
+docker run -d -p 80:80 --restart unless-stopped --network vpuu-offline  --name nginx -v $PWD/nginx:/etc/nginx nginx
 
