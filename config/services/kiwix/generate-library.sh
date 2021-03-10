6db41c8acf69ce3575a561d3b8c09472ab164d18
Dean Kayton <deankayton@gmail.com>
2021-03-10 15:24:15 +0200

    Fix library generation script

--- config/services/kiwix/generate-library.sh
+++ config/services/kiwix/generate-library.sh
@@ -2,7 +2,13 @@
 set -euf -o pipefail
 
 library_file="${KIWIX_DATA_DIR}/library.xml"
-zims_dir="${KIWIX_DATA_DIR}/zims"
-zims=(${zims_dir}/*.zim)
+complete_download_dir="${TRANSMISSION_DOWNLOADS_DIR}/complete"
+zims="$(find ${complete_download_dir} -name '*.zim' | xargs)"
 
-docker run --rm --user "$(id -u):$(id -g)" "${KIWIX_DATA_DIR}:${KIWIX_DATA_DIR}" --entrypoint bash kiwix/library -c "kiwix-manage ${library_file} add ${zims[*]}"
+docker run --rm -it \
+  --user "$(id -u):$(id -g)" \
+  -v "${TRANSMISSION_DOWNLOADS_DIR}:${TRANSMISSION_DOWNLOADS_DIR}" \
+  -v "${KIWIX_DATA_DIR}:${KIWIX_DATA_DIR}" \
+  --entrypoint bash \
+  kiwix/library \
+  -c "kiwix-manage ${library_file} add ${zims}"
