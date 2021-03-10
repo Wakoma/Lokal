4ceeadcce28e2c1e7b524fbc6419b2e99c75a61e
Dean Kayton <deankayton@gmail.com>
2021-03-10 11:33:15 +0200

    Only process completed zims

--- config/services/kiwix/generate-library.sh
+++ config/services/kiwix/generate-library.sh
@@ -3,4 +3,4 @@ set -euf -o pipefail
 
 library_file="${KIWIX_DATA_DIR}/library.xml"
 zims_dir="${KIWIX_DATA_DIR}/zims"
-docker run --workdir "${zims_dir}" --rm -iv "${KIWIX_DATA_DIR}:${KIWIX_DATA_DIR}" --entrypoint bash kiwix/library -c "kiwix-manage ${library_file} add $(ls ${zims_dir} | xargs)"
+docker run --workdir "${zims_dir}" --rm -iv "${KIWIX_DATA_DIR}:${KIWIX_DATA_DIR}" --entrypoint bash kiwix/library -c "kiwix-manage ${library_file} add $(ls ${zims_dir}/*.zim | xargs)"
