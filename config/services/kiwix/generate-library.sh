3f4bd92199ab128285f146bbe9a4dba3a75bae45
Dean Kayton <deankayton@gmail.com>
2021-05-04 13:38:35 +0200

    Copy library.xml tpl if no zims exist; Use ansible to cp .envrc

--- config/services/kiwix/generate-library.sh
+++ config/services/kiwix/generate-library.sh
@@ -13,16 +13,21 @@ complete_download_dir="${TRANSMISSION_DOWNLOADS_DIR}/complete"
 mkdir -p ${complete_download_dir}
 zims="$(find ${complete_download_dir} -name '*.zim' | xargs)"
 
-docker run --rm \
-  --user "$(id -u):$(id -g)" \
-  -v "${TRANSMISSION_DOWNLOADS_DIR}:${TRANSMISSION_DOWNLOADS_DIR}" \
-  -v "${KIWIX_DATA_DIR}:${KIWIX_DATA_DIR}" \
-  --entrypoint ash \
-  kiwix/kiwix-serve \
-  -c "kiwix-manage ${library_file} add ${zims}"
+if test -z "$zims" 
+then
+  cp library.xml.tpl "${library_file}"
+else
+  docker run --rm \
+    --user "$(id -u):$(id -g)" \
+    -v "${TRANSMISSION_DOWNLOADS_DIR}:${TRANSMISSION_DOWNLOADS_DIR}" \
+    -v "${KIWIX_DATA_DIR}:${KIWIX_DATA_DIR}" \
+    --entrypoint ash \
+    kiwix/kiwix-serve \
+    -c "kiwix-manage ${library_file} add ${zims}"
 
-echo "The following files were added to ${library_file}:"
-echo "${zims}"
-echo "Restarting Kiwix"
+  echo "The following files were added to ${library_file}:"
+  echo "${zims}"
+  echo "Restarting Kiwix"
+fi
 
 dc restart kiwix
