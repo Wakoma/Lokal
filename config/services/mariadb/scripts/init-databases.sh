f4e59198bd39c49aa41e6bf6f921a0889f27be52
Dean Kayton <deankayton@gmail.com>
2021-05-26 15:57:32 +0200

    refactor

--- config/services/mariadb/scripts/init-databases.sh
+++ config/services/mariadb/scripts/init-databases.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
 docker exec mariadb bash -c "until mysqladmin status -p${MYSQL_ROOT_PASSWORD}; do sleep 5; echo Retrying...; done"
 printf "\
 CREATE USER IF NOT EXISTS '${MYSQL_USER_WORDPRESS}'@'%%' IDENTIFIED BY '${MYSQL_PASSWORD_WORDPRESS}';\n\
