7a63605165f217e847a14dfe120a50336459ff8f
Dean Kayton <deankayton@gmail.com>
2021-05-26 14:39:23 +0200

    Hopefully sort out the ownership issues

--- config/ansible/run.sh
+++ config/ansible/run.sh
@@ -7,25 +7,28 @@ export ANSIBLE_FORCE_COLOR=true
 export ANSIBLE_VERBOSITY=0
 
 ansible-playbook \
-  --user ${ROOT_SSH_USER} \
   -e root_ssh_user=${ROOT_SSH_USER} \
   -e primary_ssh_user=${PRIMARY_SSH_USER} \
   -i ${PROJECT_ROOT}/config/ansible/hosts.yml \
   ${PROJECT_ROOT}/config/ansible/preprovision.playbook.yml
-ansible-playbook \
-  --user ${PRIMARY_SSH_USER} \
-  -e primary_ssh_user=${PRIMARY_SSH_USER} \
-  -i ${PROJECT_ROOT}/config/ansible/hosts.yml \
-  ${PROJECT_ROOT}/config/ansible/base.playbook.yml
-ansible-playbook \
-  --user ${PRIMARY_SSH_USER} \
-  -e primary_ssh_user=${PRIMARY_SSH_USER} \
-  -e repo_owner=${repo_owner} \
-  -e repo_name=${repo_name} \
-  -e repo_branch=${repo_branch} \
-  -i ${PROJECT_ROOT}/config/ansible/hosts.yml \
-  ${PROJECT_ROOT}/config/ansible/deploy-services.playbook.yml
-ansible-playbook \
-  -e primary_ssh_user=${PRIMARY_SSH_USER} \
-  -i ${PROJECT_ROOT}/config/ansible/hosts.yml \
-  ${PROJECT_ROOT}/config/ansible/deploy-jitsi.playbook.yml
+
+su ${PRIMARY_SSH_USER} -c "\
+  ansible-playbook \
+    -e primary_ssh_user=${PRIMARY_SSH_USER} \
+    -i ${PROJECT_ROOT}/config/ansible/hosts.yml \
+    ${PROJECT_ROOT}/config/ansible/base.playbook.yml"
+
+su ${PRIMARY_SSH_USER} -c "\
+  ansible-playbook \
+    -e primary_ssh_user=${PRIMARY_SSH_USER} \
+    -e repo_owner=${repo_owner} \
+    -e repo_name=${repo_name} \
+    -e repo_branch=${repo_branch} \
+    -i ${PROJECT_ROOT}/config/ansible/hosts.yml \
+    ${PROJECT_ROOT}/config/ansible/deploy-services.playbook.yml"
+
+su ${PRIMARY_SSH_USER} -c "\
+  ansible-playbook \
+    -e primary_ssh_user=${PRIMARY_SSH_USER} \
+    -i ${PROJECT_ROOT}/config/ansible/hosts.yml \
+    ${PROJECT_ROOT}/config/ansible/deploy-jitsi.playbook.yml"
