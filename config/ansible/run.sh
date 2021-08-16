b95e4fba7095bf87d2493b6da2ced0bf3f99b3cd
Dean Kayton <deankayton@gmail.com>
2021-08-16 15:57:27 +0400

    Custom/simplified Wordpress install

--- config/ansible/run.sh
+++ config/ansible/run.sh
@@ -6,6 +6,10 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_FORCE_COLOR=true
 export ANSIBLE_VERBOSITY=0
 
+export repo_owner=Wakoma
+export repo_name=Lokal
+export repo_branch=main
+
 ROOT_SSH_USER=root
 PRIMARY_SSH_USER=ubuntu
 
@@ -15,47 +19,15 @@ ansible-playbook \
   -i config/ansible/hosts.yml \
   config/ansible/preprovision.playbook.yml
 
-chown -R ${PRIMARY_SSH_USER}:${PRIMARY_SSH_USER} .
-
-su ${PRIMARY_SSH_USER} -c "ansible-galaxy role install -r config/ansible/requirements.yml"
-su ${PRIMARY_SSH_USER} -c "ansible-galaxy collection install -r config/ansible/requirements.yml"
-
-su ${PRIMARY_SSH_USER} -c "\
-  ansible-playbook \
+ansible-playbook \
     -e primary_ssh_user=${PRIMARY_SSH_USER} \
     -i config/ansible/hosts.yml \
-    config/ansible/base.playbook.yml"
+    config/ansible/base.playbook.yml
 
-su ${PRIMARY_SSH_USER} -c "\
-  ansible-playbook \
+ansible-playbook \
     -e primary_ssh_user=${PRIMARY_SSH_USER} \
     -e repo_owner=${repo_owner} \
     -e repo_name=${repo_name} \
     -e repo_branch=${repo_branch} \
     -i config/ansible/hosts.yml \
-    config/ansible/deploy-services.playbook.yml"
-
-su ${PRIMARY_SSH_USER} -c "\
-  ansible-playbook \
-    -e primary_ssh_user=${PRIMARY_SSH_USER} \
-    -e repo_name=${repo_name} \
-    -i config/ansible/hosts.yml \
-    config/ansible/deploy-jitsi.playbook.yml"
-
-su ${PRIMARY_SSH_USER} -c "\
-  ansible-playbook \
-    -e primary_ssh_user=${PRIMARY_SSH_USER} \
-    -i config/ansible/hosts.yml \
-    config/ansible/deploy-docs.playbook.yml"
-
-su ${PRIMARY_SSH_USER} -c "\
-  ansible-playbook \
-    -e primary_ssh_user=${PRIMARY_SSH_USER} \
-    -i config/ansible/hosts.yml \
-    config/ansible/deploy-azuracast.playbook.yml"
-
-su ${PRIMARY_SSH_USER} -c "\
-  ansible-playbook \
-    -e primary_ssh_user=${PRIMARY_SSH_USER} \
-    -i config/ansible/hosts.yml \
-    config/ansible/deploy-moodle.playbook.yml"
+    config/ansible/deploy-services.playbook.yml
