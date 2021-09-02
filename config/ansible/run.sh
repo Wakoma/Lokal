9171f9e98e157e89a2c3f08addbec7e01edc5a3c
Dean Kayton <deankayton@gmail.com>
2021-09-02 08:11:32 +0300

    Push custom changes

--- config/ansible/run.sh
+++ config/ansible/run.sh
@@ -8,23 +8,24 @@ export ANSIBLE_VERBOSITY=0
 
 export repo_owner=Wakoma
 export repo_name=Lokal
-export repo_branch=custom_install
+export repo_branch=custom_install_dnwt
 
 ROOT_SSH_USER=root
 PRIMARY_SSH_USER=ubuntu
 
-ansible-playbook -e "ansible_user=${ROOT_SSH_USER}" -e "setup_ssh=true" \
-  -e root_ssh_user=${ROOT_SSH_USER} \
-  -e primary_ssh_user=${PRIMARY_SSH_USER} \
-  -i config/ansible/hosts.yml \
-  config/ansible/preprovision.playbook.yml
+# Uncomment on first run, comment once run
+#ansible-playbook -e "ansible_user=${ROOT_SSH_USER}" -e "setup_ssh=true" \
+#  -e root_ssh_user=${ROOT_SSH_USER} \
+#  -e primary_ssh_user=${PRIMARY_SSH_USER} \
+#  -i config/ansible/hosts.yml \
+#  config/ansible/preprovision.playbook.yml
 
 ansible-playbook \
     -e primary_ssh_user=${PRIMARY_SSH_USER} \
     -i config/ansible/hosts.yml \
     config/ansible/base.playbook.yml
 
-read -rsp $'Setup server .envrc files, then press any key to continue...\n' -n1 key
+read -rsp $'Setup server .envrc files and lokal.network ssl certs in /tmp folder, then press any key to continue...\n' -n1 key
 
 ansible-playbook \
     -e primary_ssh_user=${PRIMARY_SSH_USER} \
@@ -33,3 +34,9 @@ ansible-playbook \
     -e repo_branch=${repo_branch} \
     -i config/ansible/hosts.yml \
     config/ansible/deploy-services.playbook.yml
+
+ansible-playbook \
+    -e primary_ssh_user=${PRIMARY_SSH_USER} \
+    -e repo_name=${repo_name} \
+    -i config/ansible/hosts.yml \
+    config/ansible/deploy-jitsi.playbook.yml
