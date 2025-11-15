1daa483da13b4efa4845367a1688b8b67bf747c5
Tomas Peterka <tomas@peterka.me>
2025-11-15 09:19:38 +0100

    feat: finish the split

--- port-docs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-for file in $(ls roles/docs/docs/docs/services/); do
-    if [[ -e ../services/roles/$file ]]; then
-        rm -rf roles/docs/docs/docs/services/$file
-    fi
-done
